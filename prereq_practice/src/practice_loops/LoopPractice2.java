4bcfc60178e8f22a151f3a54d780a4c298e394e3
Andrew D'Ambrosio <adambrosio20@gmail.com>
2021-04-27 11:21:31 -0400

    java practice loops and strings

---
+++ prereq_practice/src/practice_loops/LoopPractice2.java
@@ -0,0 +1,14 @@
+package practice_loops;
+
+public class LoopPractice2 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 100; i += 2) {
+				System.out.println(i);
+			
+		}
+
+	}
+
+}
---
+++ prereq_practice/src/practice_loops/LoopPractice3.java
@@ -0,0 +1,15 @@
+package practice_loops;
+
+public class LoopPractice3 {
+
+	public static void main(String[] args) {
+
+		for (int idx = 0; idx < 100; idx++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.println("the value of idx was " + idx + " ------- " + j);
+			}
+		}
+
+	}
+
+}
